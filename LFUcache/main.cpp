11c1d9888f191574b3ab2e77627526208a75ce60
Amasterworld <nvthao147@gmail.com>
2022-07-22 20:57:10 +0200

    Initial Commit

---
+++ LFUcache/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LFUcache
+//
+//  Created by Phuong Duong Bich on 22.07.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
